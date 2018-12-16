a86cb566964b0e66eff5b72969e3b8aa38a12305
franc <franc@LAPTOP-SHQCQ4GH>
2018-12-16 01:22:14 -0600

    Vistas

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,14 +5,28 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
 
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+    public function indexAction(Request $request) {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('role_check');
+        }
+        return $this->render('AppBundle:Default:index.html.twig', array());
     }
+
+    public function loginAction(Request $request) {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('role_check');
+        }
+        return $this->render("AppBundle:Default:index.html.twig", []);
+    }
+
+    public function roleCheckAction(Request $request) {
+        if ($this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('app_users');
+        } else if ($this->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('app_user_profile');
+        }
+    }
+    
 }
